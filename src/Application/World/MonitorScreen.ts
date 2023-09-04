85b7353d793ed15705fd593371c3dc659fb65ed3
Sneh Acharya <20356sneh@gmail.com>
2023-09-04 17:53:05 -0500

    FIXED ISSUE

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://acharyasneh.com';
+        iframe.src = 'https://snehinnersite.netlify.app';
         /**
          * Use dev server is query params are present
          *
